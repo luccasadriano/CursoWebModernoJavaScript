bb28026eb43b1088703391313db761a65241ce14
luccasadriano <luccas-adriano@hotmail.com>
2020-07-06 01:44:43 -0300

    iniciando seção 8: Node

---
+++ Exercicios js/Node/modeloA.js
@@ -0,0 +1,5 @@
+console.log(this)
+
+this.ola = 'Fala Pessoal'
+exports.BemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até próximo exemplo'
---
+++ Exercicios js/Node/moduloCliente.js
@@ -0,0 +1,13 @@
+const modeloA = require('./modeloA')
+const modeloB = require('./modeloB')
+
+console.log(modeloA.ola)
+console.log(modeloA.BemVindo)
+console.log(modeloA.ateLogo)
+console.log(modeloA)
+
+console.log(modeloB.bomDia)
+console.log(modeloB.boaNoite())
+console.log(modeloB)
+
+
